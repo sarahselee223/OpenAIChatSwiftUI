a6b74138a74accf2985862bcc248d5f71f149a27
Sarah Lee <sarahselee223@gmail.com>
2023-01-31 20:48:19 -0800

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Sarah Lee on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
